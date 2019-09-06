f5865b7de4b991710ce66cc967f3cc192ca98426
Gary <gary@iMacA11.local>
2019-09-06 14:00:30 +0700

    end of section 3

--- 3-how-JS-works/starter/script.js
+++ 3-how-JS-works/starter/script.js
@@ -1,21 +1,33 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
+/*
+calculateAge(1965); // works as its a function declaration
+
+function calculateAge(year) {
+    console.log(2016 - year);
+}
+
+// retirement(1990); does not work as it's a function expression
+
+var retirement = function(year) {
+    console.log(65 - (2016 - year));
+}
 
 
+// variables
 
+console.log(age); // undefined as variables are declared undefined until called
+var age = 23;
+console.log(age);
 
-
-
-
-
-
-
-
-
-
-
-
+function foo() {
+    var age = 65;
+    console.log(age); // foo variable of age
+}
+foo();
+console.log(age); // global variable of age
+*/
 
 ///////////////////////////////////////
 // Lecture: Scoping
@@ -52,25 +64,57 @@ function first() {
 
     function second() {
         var c = 'Hey!';
-        third()
+        third()             //can access function 3 because it's in the scope
     }
 }
 
 function third() {
     var d = 'John';
-    console.log(a + b + c + d);
+    console.log(a + b + c + d); //cannot access first() and second() variables
 }
-*/
 
+*/
 
 
 ///////////////////////////////////////
 // Lecture: The this keyword
 
+//console.log(this);
+/*
+calculateAge(1985);
 
+function calculateAge(year) {
+    console.log(2016 - year);
+    console.log(this);   // this still references the window object
+}
+*/
+var john = {
+    name: 'John',
+    yearOfBirth: 1990,
+    calculateAge: function() {
+        console.log(this); // this references the john object
+        console.log(2016 - this.yearOfBirth);
+        /*
+        function innerFunction() {
+            console.log(this);
+        }
+        innerFunction();
+        */
+    }
+}
 
+john.calculateAge();
 
+var mike = {
+    name: 'Mike',
+    yearOfBirth: 1984,
 
+};
+
+// method borrowing
+mike.calculateAge = john.calculateAge;
+mike.calculateAge();
+console.log(mike.name + ' is ' + mike.calculateAge());
 
 
 
